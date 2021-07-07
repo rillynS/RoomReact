8bea6619cc700755298d8d89188f53b3e69b25fb
start prj <POM@M.SV-PLUS.RU>
2021-07-07 19:31:35 +0200

    start prj

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Layout from './hoc/Layout/Layout';
+
+class App extends Component {
+    render() {
+        return(
+            <Layout>
+                <div>app</div>
+            </Layout>
+        )
+    }
+}
+
+export default App
